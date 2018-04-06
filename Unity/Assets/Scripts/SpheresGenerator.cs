4ac07cf0dad65e22780877f22ddba2e42be2cbe8
jamesyang1025 <36896710+jamesyang1025@users.noreply.github.com>
2018-04-06 18:01:15 -0500

    Added CSV parsing scripts. Generated spheres according to the XYZ coordinates in the CSV files

--- Unity/Assets/Scripts/SpheresGenerator.cs
+++ Unity/Assets/Scripts/SpheresGenerator.cs
@@ -4,9 +4,24 @@ using UnityEngine;
 
 public class SpheresGenerator : MonoBehaviour {
 
+    private int[] objects;
+    public CSV CSVManager;
+
 	// Use this for initialization
 	void Start () {
-		
+
+
+        Debug.Log(CSVManager.NumRows());
+
+        
+        for(int i = 0; i < CSVManager.NumRows(); i++)
+        {
+            GameObject sp = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            sp.transform.position = new Vector3(float.Parse(CSVManager.GetRowList()[i].X), float.Parse(CSVManager.GetRowList()[i].Y), 
+                                                float.Parse(CSVManager.GetRowList()[i].Z));
+
+        }
+        
 	}
 	
 	// Update is called once per frame
