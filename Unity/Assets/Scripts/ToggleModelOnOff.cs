e84faa5664a81741fb0641966fbdec0193f89abe
jamesyang1025 <jamesyang1025@gmail.com>
2019-02-28 16:50:43 -0600

    Added UI button to turn the model on & off

---
+++ Unity/Assets/Scripts/ToggleModelOnOff.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleModelOnOff : MonoBehaviour {
+
+	private GameObject model;
+
+	// Use this for initialization
+	void Start () {
+		model = this.gameObject.transform.GetChild(0).gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*
+		if(Input.GetKeyDown(KeyCode.O)){
+			Debug.Log("Set model state");
+			model.SetActive(!model.activeSelf);
+		}
+		*/
+	}
+
+	public void ModelOnOff(){
+		model.SetActive(!model.activeSelf);
+	}
+}
