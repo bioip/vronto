beaebfa1a5b9c96784676983db571aae1ebcadb1
jamesyang1025 <36896710+jamesyang1025@users.noreply.github.com>
2019-04-21 22:12:41 -0500

    Polish UI layout

---
+++ Unity/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+	public GameObject shelfPage;
+	public GameObject spherePage;
+	public GameObject modelPage;
+	public GameObject homePage;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnShelfPage(){
+		shelfPage.SetActive(true);
+		spherePage.SetActive(false);
+		modelPage.SetActive(false);
+		homePage.SetActive(false);
+	}
+
+	public void OnSpherePage(){
+		shelfPage.SetActive(false);
+		spherePage.SetActive(true);
+		modelPage.SetActive(false);
+		homePage.SetActive(false);
+	}
+
+	public void OnModelPage(){
+		shelfPage.SetActive(false);
+		spherePage.SetActive(false);
+		modelPage.SetActive(true);
+		homePage.SetActive(false);
+	}
+
+	public void OnHomePage(){
+		shelfPage.SetActive(false);
+		spherePage.SetActive(false);
+		modelPage.SetActive(false);
+		homePage.SetActive(true);
+	}
+}
