a4d2ec9c114189de8b2ddea4011b48bb79e30a37
jamesyang1025 <36896710+jamesyang1025@users.noreply.github.com>
2019-05-01 16:42:02 -0500

    Changed some text color from to green & Add functionality to turn HUD on/off by gripping controller

--- Unity/Assets/Scripts/HUDOnOff.cs
+++ Unity/Assets/Scripts/HUDOnOff.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using VRTK;
 
 public class HUDOnOff : MonoBehaviour {
 
@@ -10,6 +11,11 @@ public class HUDOnOff : MonoBehaviour {
 	private GameObject input;
 	private GameObject keys;
 	private GameObject text;
+	private GameObject rightController;
+    private GameObject leftController;
+    private VRTK_ControllerEvents leftControllerEvents;
+    private VRTK_ControllerEvents rightControllerEvents;
+	private bool buttonPressed;
 
 	// Use this for initialization
 	void Start () {
@@ -18,11 +24,24 @@ public class HUDOnOff : MonoBehaviour {
 		input = keyboard.transform.GetChild(1).gameObject;
 		keys = keyboard.transform.GetChild(2).gameObject;
 		text = keyboard.transform.GetChild(3).gameObject;
+
+		leftController = GameObject.FindGameObjectWithTag("LeftController");
+        leftControllerEvents = leftController.GetComponent<VRTK_ControllerEvents>();
+        rightController = GameObject.FindGameObjectWithTag("RightController");
+        rightControllerEvents = rightController.GetComponent<VRTK_ControllerEvents>();
+		buttonPressed = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(leftControllerEvents.IsButtonPressed(VRTK_ControllerEvents.ButtonAlias.GripPress) || rightControllerEvents.IsButtonPressed(VRTK_ControllerEvents.ButtonAlias.GripPress)){
+			if(!buttonPressed){
+				ToggleOnOff();
+			}
+			buttonPressed = true;
+		}else{
+			buttonPressed = false;
+		}
 	}
 
 	public void ToggleOnOff(){
