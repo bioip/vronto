76b1b5f4ea0608f53b0eda9b151b3b5c510bd8d6
jamesyang1025 <36896710+jamesyang1025@users.noreply.github.com>
2019-05-01 14:30:05 -0500

    Update searchbar function for new dropdown feature

--- Unity/Assets/Scripts/SearchBar.cs
+++ Unity/Assets/Scripts/SearchBar.cs
@@ -10,8 +10,10 @@ public class SearchBar : MonoBehaviour
     private List<Dropdown.OptionData> m_Messages;
     private InputField input;
     private List<string> labels = new List<string>();
+    private List<string> m_dropdownList;
     private string outputString;
     private bool matched;
+    public GameObject SphereGenerator;
 
     // Use this for initialization
     void Start()
@@ -24,6 +26,9 @@ public class SearchBar : MonoBehaviour
         }
         matched = false;
 
+        m_dropdownList = SphereGenerator.GetComponent<SpheresGenerator>().m_dropdownList;
+
+        Debug.Log(m_dropdownList.IndexOf("head"));
     }
 
     public void ClickKey()
@@ -33,13 +38,17 @@ public class SearchBar : MonoBehaviour
             //DropdownList.Show();
             //outputString = input.text;
             matched = false;
-            foreach (string label in labels)
+            foreach (string label in m_dropdownList)
             {
-                if (label.StartsWith(input.text.Trim()))
+                if (label.ToUpper().Trim().StartsWith(input.text.Trim()))
                 {
                     if (!matched)
                     {
                         DropdownList.value = labels.IndexOf(label);
+                        int startIndex = m_dropdownList.IndexOf(label);
+                        int range = Mathf.Min(20, m_dropdownList.Count - startIndex);
+                        DropdownList.ClearOptions();
+                        DropdownList.AddOptions(m_dropdownList.GetRange(startIndex, range));
                         matched = true;
                     }
                 }
@@ -55,5 +64,7 @@ public class SearchBar : MonoBehaviour
 			DropdownList.value = findValue;
 		}
 		*/
+
+
     }
 }
